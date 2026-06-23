4e487bd754caf39c4f94f77c8ebe076e7216ad74
Kate <nechyporenkok@gmail.com>
2026-06-23 15:44:07 +0100

    update TS

--- Fig1_TS.m
+++ Fig1_TS.m
@@ -1,7 +1,7 @@
 clc; clearvars; close all
 
 % Add function path
-addpath(genpath('/home/kn356/Desktop/SNIC2/functions'))
+addpath(genpath('\home\kn356\Desktop\SNIC2\functions'))
 
 % Load model parameters
 load("wc_par_SNIC2_noise.mat")  % Loads struct `p`
@@ -13,8 +13,8 @@ X0 = [0.0; 0.0];     % Initial condition
 p.noise_std = 0.01;
 
 %% Bistable regime
-p.Kp = 1.075;
-p.cie =7.76;
+p.Kp = 1.1;
+p.cie =7.8;
 [t, X] = simulate_wc_sde(p, X0, dt, T);
 
 if exist('f2','var') && isgraphics(f2)
@@ -24,7 +24,7 @@ f2 = figure(2); f2.Units = "centimeters"; f2.OuterPosition = [10 30 16 12];
 set(gca, 'FontSize', 16, 'FontName', 'Times'); hold on; box on;
 xlabel('Time', 'Interpreter', 'latex')
 ylabel('Population activity', 'Interpreter', 'latex')
-plot(t, X(2,:), 'k', 'LineWidth', 2)
+plot(t, X(1,:), 'k', 'LineWidth', 2)
 ylim([-0.1 0.6])
 % saveas(f2, 'figure_components/f1/ts_bi.svg', 'svg')
 
@@ -42,13 +42,13 @@ f3 = figure(3); f3.Units = "centimeters"; f3.OuterPosition = [10 30 16 12];
 set(gca, 'FontSize', 16, 'FontName', 'Times'); hold on; box on;
 xlabel('Time', 'Interpreter', 'latex')
 ylabel('Population activity', 'Interpreter', 'latex')
-plot(t, X(2,:), 'k', 'LineWidth', 2)
+plot(t, X(1,:), 'k', 'LineWidth', 2)
 ylim([-0.1 0.6])
 % saveas(f3, 'figure_components/f1/ts_down.svg', 'svg')
 
 %% Up state
-p.Kp = 1.48;
-p.cie =8.36;
+p.Kp = 1.44;
+p.cie =8.35;
 p.noise_std = 0.02;
 X0 = [0.5; 0.5];
 if exist('f4','var') && isgraphics(f4)
@@ -61,14 +61,14 @@ f4 = figure(4); f4.Units = "centimeters"; f4.OuterPosition = [10 30 16 12];
 set(gca, 'FontSize', 16, 'FontName', 'Times'); hold on; box on;
 xlabel('Time', 'Interpreter', 'latex')
 ylabel('Population activity', 'Interpreter', 'latex')
-plot(t, X(2,:), 'k', 'LineWidth', 2)
+plot(t, X(1,:), 'k', 'LineWidth', 2)
 ylim([-0.1 0.6])
 %
 saveas(f4, 'figure_components/f1/ts_up.svg', 'svg')
 
 %% Limit cycle regime
-p.Kp = 1.15;
-p.cie =10;
+p.Kp = 1.25;
+p.cie =9.3;
 X0 = [0.0; 0.0];
 p.noise_std = 0.01;
 T = 100;  % Shorter time for oscillations
@@ -81,6 +81,6 @@ f5 = figure(5); f5.Units = "centimeters"; f5.OuterPosition = [50 40 16 12];
 set(gca, 'FontSize', 16, 'FontName', 'Times'); hold on; box on;
 xlabel('Time', 'Interpreter', 'latex')
 ylabel('Population activity', 'Interpreter', 'latex')
-plot(t, X(2,:), 'k', 'LineWidth', 2)
+plot(t, X(1,:), 'k', 'LineWidth', 2)
 ylim([-0.1 0.6])
 saveas(f5, 'figure_components/f1/ts_LC.svg', 'svg')
